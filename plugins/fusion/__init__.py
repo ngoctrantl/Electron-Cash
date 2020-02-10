5ed2a7273d89563799243bc93855e27fc792290f
Calin Culianu <calin.culianu@gmail.com>
2020-02-11 00:47:58 +0200

    i18n: Internal plugins were appearing untranslated, fixed.
    This is because the _() decorator they were using in __init__.py wasn't
    able to get the proper language string (the language is not set when
    internal plugins are loaded).
    
    So we had to write some workaround code to retranslate the pluign
    "fullname" and "description" text each time before the plugin settings
    dialog is shown.
    
    Also added support for plugin "description" to be a list of _() strings
    (so that the translation works properly if done later).

--- plugins/fusion/__init__.py
+++ plugins/fusion/__init__.py
@@ -28,13 +28,14 @@
 from electroncash.i18n import _
 
 fullname = _('CashFusion')
-description = ''.join([
+description = [
     _('Protect your privacy and anonymize your coins (UTXOs) by shuffling them with other users of CashFusion.'),
     "\n\n",
     _('A commitment and anonymous announcement scheme is used so that none of the participants know the inputs nor '
       'outputs of the other participants.'), " ",
     _('In addition, a blame protocol is used to mitigate time-wasting denial-of-service type attacks.')
-])
+]
+description_delimiter = ''
 available_for = ['qt', 'cmdline']
 # If default_on is set to True, this plugin is loaded by default on new installs
 default_on = True
